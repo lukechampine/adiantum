be9c3936168ba9da2fdaa3531a289f22a656d551
lukechampine <luke.champine@gmail.com>
2019-02-10 14:33:05 -0500

    hpolyc: Implement XChaCha12 variant

--- hpolyc/hpolyc.go
+++ hpolyc/hpolyc.go
@@ -8,6 +8,23 @@ import (
 	"lukechampine.com/adiantum/hbsh"
 )
 
+// xchacha12Stream implements hbsh.StreamCipher with XChaCha12.
+type xchacha12Stream []byte
+
+// XORKeyStream implements hbsh.StreamCipher.
+func (key xchacha12Stream) XORKeyStream(nonce, dst, src []byte) {
+	// expand nonce with HChaCha12
+	var tmpKey [32]byte
+	var hNonce [16]byte
+	copy(hNonce[:], nonce[:16])
+	copy(tmpKey[:], key)
+	hChaCha12(&tmpKey, &hNonce, &tmpKey)
+	chacha.XORKeyStream(dst, src, nonce[16:], tmpKey[:], 12)
+}
+
+// NonceSize implements hbsh.StreamCipher.
+func (xchacha12Stream) NonceSize() int { return chacha.XNonceSize }
+
 // xchacha20Stream implements hbsh.StreamCipher with XChaCha20.
 type xchacha20Stream []byte
 
@@ -32,6 +49,20 @@ func (key hashPoly1305) Sum(dst, src []byte) []byte {
 // New returns an HPolyC cipher with the specified key.
 func New(key []byte) *hbsh.HBSH {
 	// create stream cipher and derive block+hash keys
+	stream := xchacha12Stream(key)
+	keyBuf := make([]byte, 32+16)
+	nonce := make([]byte, chacha.XNonceSize)
+	nonce[0] = 1
+	stream.XORKeyStream(nonce, keyBuf, keyBuf)
+	block, _ := aes.NewCipher(keyBuf[:32])
+	var hash hashPoly1305
+	copy(hash[:16], keyBuf[32:])
+	return hbsh.New(stream, block, hash)
+}
+
+// New20 returns an HPolyC cipher with the specified key, using XChaCha20 as the
+// stream cipher.
+func New20(key []byte) *hbsh.HBSH {
 	stream := xchacha20Stream(key)
 	keyBuf := make([]byte, 32+16)
 	nonce := make([]byte, chacha.XNonceSize)
--- hpolyc/hpolyc_test.go
+++ hpolyc/hpolyc_test.go
@@ -12,25 +12,33 @@ func fromHex(s string) []byte {
 	return b
 }
 
-func TestHPolyC_XChaCha20_32_AES256(t *testing.T) {
-	// Only 100 test vectors are included. To test the full set, replace this
-	// file with the corresponding file from github.com/google/adiantum.
-	js, err := ioutil.ReadFile("testdata/HPolyC_XChaCha20_32_AES256.json")
+type testVector struct {
+	Description string `json:"description"`
+	Input       struct {
+		Key   string `json:"key_hex"`
+		Tweak string `json:"tweak_hex"`
+	} `json:"input"`
+	Plaintext  string `json:"plaintext_hex"`
+	Ciphertext string `json:"ciphertext_hex"`
+}
+
+func readTestVectors(t *testing.T, filename string) []testVector {
+	t.Helper()
+	js, err := ioutil.ReadFile(filename)
 	if err != nil {
 		t.Fatal(err)
 	}
-	var tests []struct {
-		Description string `json:"description"`
-		Input       struct {
-			Key   string `json:"key_hex"`
-			Tweak string `json:"tweak_hex"`
-		} `json:"input"`
-		Plaintext  string `json:"plaintext_hex"`
-		Ciphertext string `json:"ciphertext_hex"`
-	}
+	var tests []testVector
 	if err := json.Unmarshal(js, &tests); err != nil {
 		t.Fatal(err)
 	}
+	return tests
+}
+
+func TestHPolyC_XChaCha12_32_AES256(t *testing.T) {
+	// Only 100 test vectors are included. To test the full set, replace this
+	// file with the corresponding file from github.com/google/adiantum.
+	tests := readTestVectors(t, "testdata/HPolyC_XChaCha12_32_AES256.json")
 	for i, test := range tests {
 		hpc := New(fromHex(test.Input.Key))
 		ciphertext := hpc.Encrypt(fromHex(test.Plaintext), fromHex(test.Input.Tweak))
@@ -44,7 +52,24 @@ func TestHPolyC_XChaCha20_32_AES256(t *testing.T) {
 	}
 }
 
-func BenchmarkHPolyC(b *testing.B) {
+func TestHPolyC_XChaCha20_32_AES256(t *testing.T) {
+	// Only 100 test vectors are included. To test the full set, replace this
+	// file with the corresponding file from github.com/google/adiantum.
+	tests := readTestVectors(t, "testdata/HPolyC_XChaCha20_32_AES256.json")
+	for i, test := range tests {
+		hpc := New20(fromHex(test.Input.Key))
+		ciphertext := hpc.Encrypt(fromHex(test.Plaintext), fromHex(test.Input.Tweak))
+		if hex.EncodeToString(ciphertext) != test.Ciphertext {
+			t.Fatalf("%v (%v): Encryption failed:\nexp: %v\ngot: %x", test.Description, i, test.Ciphertext, ciphertext)
+		}
+		plaintext := hpc.Decrypt(fromHex(test.Ciphertext), fromHex(test.Input.Tweak))
+		if hex.EncodeToString(plaintext) != test.Plaintext {
+			t.Fatalf("%v (%v): Decryption failed:\nexp: %v\ngot: %x", test.Description, i, test.Plaintext, plaintext)
+		}
+	}
+}
+
+func BenchmarkHPolyC_XChaCha12_32_AES256(b *testing.B) {
 	b.Run("Encrypt", func(b *testing.B) {
 		block := make([]byte, 4096)
 		tweak := make([]byte, 12)
@@ -59,6 +84,29 @@ func BenchmarkHPolyC(b *testing.B) {
 		block := make([]byte, 4096)
 		tweak := make([]byte, 12)
 		hpc := New(make([]byte, 32))
+		b.SetBytes(int64(len(block)))
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			hpc.Decrypt(block, tweak)
+		}
+	})
+}
+
+func BenchmarkHPolyC_XChaCha20_32_AES256(b *testing.B) {
+	b.Run("Encrypt", func(b *testing.B) {
+		block := make([]byte, 4096)
+		tweak := make([]byte, 12)
+		hpc := New20(make([]byte, 32))
+		b.SetBytes(int64(len(block)))
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			hpc.Encrypt(block, tweak)
+		}
+	})
+	b.Run("Decrypt", func(b *testing.B) {
+		block := make([]byte, 4096)
+		tweak := make([]byte, 12)
+		hpc := New20(make([]byte, 32))
 		b.SetBytes(int64(len(block)))
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
